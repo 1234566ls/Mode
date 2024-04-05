978c7e43eeb05da8575e5c445126cf0d25f7c97e
unknown <429195-8>
2024-04-05 16:30:47 +0300

    Работа

--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.
 
 namespace WindowsFormsApp1
 {
@@ -20,7 +21,7 @@ namespace WindowsFormsApp1
 
         private void button1_Click(object sender, EventArgs e)
         {
-            string connectionString = @"Data Source=ADCLG13;Initial Catalog=movie_DB;Integrated Security=True";
+            string connectionString = @"Data Source=ADCLG1;Initial Catalog=movie_DB;Integrated Security=True";
 
             // Создание подключения
             SqlConnection connection = new SqlConnection(connectionString);
@@ -41,7 +42,37 @@ namespace WindowsFormsApp1
                 MessageBox.Show("Подключение закрыто...");
             }
 
+            string sqlExpression = "SELECT * FROM Users";
+            
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(sqlExpression, connection);
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.HasRows) // если есть данные
+                {
+                    // выводим названия столбцов
+                    Console.WriteLine("{0}\t{1}\t{2}\t{3}\t{4}", reader.GetName(0), reader.GetName(1), reader.GetName(2), reader.GetName(3), reader.GetName(4));
+
+                    while (reader.Read()) // построчно считываем данные
+                    {
+                        object id = reader.GetValue(0);
+                        object name = reader.GetValue(1);
+                        object Sumame = reader.GetValue(2);
+                        double Salary = reader.GetDouble(3);
+                        object Department_id = reader.GetValues(4);
+
+
+                        Console.WriteLine("{0} \t{1} \t{2} \t{3} \t{4}", id, name, Sumame, Salary, Department_id);
+                    }
+                }
+
+                reader.Close();
+            }
+
             Console.Read();
         }
+    }
+
     }
 }
